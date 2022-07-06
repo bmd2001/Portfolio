1110563fc56253892e19c2be027f2bddb77637de
brexit2001 <davidebressani21@gmail.com>
2022-07-06 19:08:13 +0100

    Added ways to decode my email adress and phone number

---
+++ app/js/decoding.js
@@ -0,0 +1,10 @@
+function encode(element, type){
+    switch(type){
+        case 0:
+            window.location.href = 'mailto:' + he.decode(atob("JiN4NjQ7JiN4NjY7JiN4NjI7JiN4MzU7JiN4MzA7JiN4MzU7JiN4NDA7JiN4Nzk7JiN4NmY7JiN4NzI7JiN4NmI7JiN4MmU7JiN4NjE7JiN4NjM7JiN4MmU7JiN4NzU7JiN4NmI7"));
+            break;
+        case 1:
+            window.location.href = 'tel:' + he.decode(atob("JiN4MmI7JiN4MzM7JiN4Mzk7JiN4MzM7JiN4MzQ7JiN4Mzc7JiN4MzE7JiN4Mzk7JiN4Mzc7JiN4MzU7JiN4MzY7JiN4MzA7JiN4MzU7"));
+            break;
+    }
+}
